fc28f407df87e424988caf1cd49bb08617ea26f5
nwb18 <nwbrantly@pitt.edu>
2024-10-28 14:30:50 -0400

    Update C3 data transfer script
    Update C3 data transfer script to automatically create all server data folders (including 'PC2' folders) if they do not already exist. store base directory names in variable for brevity and clarity. NWB

--- Protocol_C3/TransferData_C3.m
+++ Protocol_C3/TransferData_C3.m
@@ -14,31 +14,41 @@ else                    % otherwise, ...
     sess = 'Session2';  % session two
 end
 
+dataPC1 = fullfile(['C:\Users\Public\Documents\Vicon Training\' ...
+    'Stroke_CCC'],participantID,sess);
+dataSrvr = fullfile('W:\Nathan\C3\Data',participantID);
+
 % Define source and destination paths in specified order
-paths = {
-    fullfile('C:\Users\Public\Documents\Vicon Training\Stroke_CCC', ...
-    participantID,sess) fullfile('W:\Nathan\C3\Data',participantID, ...
-    sess,'PC1');
+pathsTransfer = {
+    dataPC1 fullfile(dataSrvr,sess,'PC1');
     fullfile(['C:\Users\Public\Documents\MATLAB\ExperimentalGUI\' ...
-    'profiles\Stroke_CCC'],participantID) fullfile('W:\Nathan\C3\Data', ...
-    participantID,'Profiles');
-    fullfile('C:\Users\Public\Documents\Vicon Training\Stroke_CCC', ...
-    participantID,sess) fullfile('W:\Nathan\C3\RawBackupData', ...
-    participantID,sess,'PC1')
+    'profiles\Stroke_CCC'],participantID) fullfile(dataSrvr,'Profiles');
+    dataPC1 fullfile('W:\Nathan\C3\RawBackupData',participantID,sess,'PC1')
+    };
+
+pathsCreate = {
+    fullfile(dataSrvr,sess,'DataLogsUnsyncd')
+    fullfile(dataSrvr,sess,'Figures')
+    fullfile(dataSrvr,sess,'PC2')
+    fullfile(dataSrvr,sess,'SyncFiles')
+    fullfile(dataSrvr,sess,'SyncFiles')
+    fullfile('W:\Nathan\C3\RawBackupData',participantID,sess,'PC2')
     };
 
 % TODO: add datalog automatic transfer by checking date modified / filename
 % 'C:\Users\Public\Documents\MATLAB\ExperimentalGUI\datlogs'
 % 'W:\Nathan\C3\Data\PilotSL\Session1\DataLogsUnsyncd'
-% TODO: automatically create the folders for PC2 if they do not already
-% exist even though those will need to be manually transferred using other
-% computer
 
-% Loop through each source-destination pair and initiate recursive copy
-for p = 1:size(paths,1)     % for each source-destination pair, ...
-    srcDir = paths{p,1};    % source directory
-    destDir = paths{p,2};   % destination directory
+for p = 1:size(pathsTransfer,1)     % for each source-destination pair, ...
+    srcDir = pathsTransfer{p,1};    % source directory
+    destDir = pathsTransfer{p,2};   % destination directory
     fprintf('Transferring from %s to %s\n',srcDir,destDir);
     utils.transferData(srcDir,destDir); % call recursive data transfer fxn
+end
+
+for p = 1:length(pathsCreate)       % for each path to create, ...
+    if ~isfolder(pathsCreate{p})    % if does not already exist, ...
+        mkdir(pathsCreate{p});      % create it
+    end
 end
 
