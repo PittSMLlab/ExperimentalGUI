9199fcb61e675d1e363110af143c9f28879a8390
nwb18 <nwbrantly@pitt.edu>
2024-11-09 13:59:48 -0500

    Update formatting
    Make minor formatting and comment modifications for clarity and to facilitate file comparison with updated Arduino stimulation code verion. Add 'TODO' notes for future considerations. NWB

--- HreflexStimArduino/Dual_Stims_GaitEventDetection/Dual_Stims_GaitEventDetection.ino
+++ HreflexStimArduino/Dual_Stims_GaitEventDetection/Dual_Stims_GaitEventDetection.ino
@@ -1,4 +1,4 @@
-// Code to stimulate the tibial nerve of both legs for Spinal Adaptation
+// Code to stimulate the tibial nerve of both legs for the Spinal Adaptation
 // study to measure H-reflexes during split-belt adaptation
 // date (started): 20 Mar. 2024
 // author(s): SL, NWB
@@ -12,24 +12,26 @@ float leftTreadmillSpeed = 0.8;   // Left treadmill belt speed [m/s]
 int rightDelayTime = 200;         // Heel strike delay for right leg [ms]
 int leftDelayTime = 200;          // Heel strike delay for left leg [ms]
 
-// variables to track gait events and phases
-bool new_stanceL = false;
-bool new_stanceR = false;
-bool old_stanceL = false;
-bool old_stanceR = false;
-bool LHS = false;                 // Left heel strike
-bool RHS = false;                 // Right heel strike
-bool LTO = false;                 // Left toe off
-bool RTO = false;                 // Right toe off
-bool canStim = false;             // Flag to enable stimulation
-// Gait phase: 0 = double support, 1 = single L support, 2 = single R support
+// initialize variables to track gait events and phases
+bool new_stanceL = false;         // is current step left foot stance?
+bool new_stanceR = false;         // is current step right foot stance?
+bool old_stanceL = false;         // is previous step left foot stance?
+bool old_stanceR = false;         // is previous step right foot stance?
+bool LHS = false;                 // is there a left heel strike event?
+bool RHS = false;                 // is there a right heel strike event?
+bool LTO = false;                 // is there a left toe off event?
+bool RTO = false;                 // is there a right toe off event?
+bool canStim = false;             // is stimulation allowed at this time?
+// gait phase: 0 = initial double support, 1 = single L support, 2 = single R
+// support, 3 = double support from single L support, 4 = double support from
+// single R support
 int phase = 0;
-int LstepCount = 0;               // Left step counter
-int RstepCount = 0;               // Right step counter
+int LstepCount = 0;               // left step counter
+int RstepCount = 0;               // right step counter
 
-const int threshFz = 5;           // Force thresh. [N] to detect stance phase
-const int numStrides = 10;        // Number of steps after which to stimulate
-const int stimPulseDuration = 20; // Stimulation pulse duration [ms]
+const int threshFz = 5;           // force thresh. [N] to detect stance phase
+const int numStrides = 10;        // number of strides after which to stimulate
+const int stimPulseDuration = 20; // stimulation pulse duration [ms]
 
 // right and left stimulator pin configurations
 const int rightSensorPin = A1;
@@ -50,96 +52,103 @@ void loop() {
   old_stanceL = new_stanceL;
   old_stanceR = new_stanceR;
 
-  // read sensor values to detect new stance phase
+  // read z-axis force plate sensor values to detect new stance phase
   float rightSensorVal = analogRead(rightSensorPin);
   float leftSensorVal = analogRead(leftSensorPin);
 
+  // current step is stance if foot in contact with force plate
   new_stanceL = leftSensorVal > threshFz;   // detect left stance
   new_stanceR = rightSensorVal > threshFz;  // detect right stance
 
-  LHS = new_stanceL && !old_stanceL;        // Left heel strike detection
-  RHS = new_stanceR && !old_stanceR;        // Right heel strike detection
-  LTO = !new_stanceL && old_stanceL;        // Left toe off detection
-  RTO = !new_stanceR && old_stanceR;        // Right toe off detection
+  LHS = new_stanceL && !old_stanceL;        // left heel strike detection
+  RHS = new_stanceR && !old_stanceR;        // right heel strike detection
+  LTO = !new_stanceL && old_stanceL;        // left toe off detection
+  RTO = !new_stanceR && old_stanceR;        // right toe off detection
 
-  // gait state machine to determine phase transitions
+  // gait event state machine to determine phase transitions
   switch (phase) {
-    case 0:  // Initial double support phase
-      if (RTO) {           // If right toe off, enter left single stance
+    case 0:                     // initial double support phase
+      if (RTO) {                // if right toe off, enter left single stance
         phase = 1;
-        RstepCount++;      // Increment right step count
+        RstepCount++;           // increment right step count
         Serial.print("Right Step: ");
         Serial.println(RstepCount);
-      } else if (LTO) {    // If left toe off, enter right single stance
+      } else if (LTO) {         // if left toe off, enter right single stance
         phase = 2;
-        LstepCount++;      // Increment left step count
+        LstepCount++;           // increment left step count
         Serial.print("Left Step: ");
         Serial.println(LstepCount);
       }
       break;
 
-    case 1:  // Left single stance
-      if (RHS) {           // If right heel strike, enter double stance
+    case 1:                     // left single stance
+      if (RHS) {                // if right heel strike, enter double stance
         phase = 3;
+        // TODO: consider reimplementing if beneficial
         // if (LTO) {
         //   // if missed entire phase switch due to short double stance
         //   phase = 2;
         //   LstepCount++;
         // }
-        canStim = true;    // Enable stimulation
+        canStim = true;         // enable stimulation
       }
       break;
 
-    case 2:  // Right single stance
-      if (LHS) {           // If left heel strike, enter double stance
+    case 2:                     // right single stance
+      if (LHS) {                // if left heel strike, enter double stance
         phase = 4;
+        // TODO: consider reimplementing if beneficial
         // if (RTO) {
         //   // if missed entire phase switch due to short double stance
         //   phase = 1;
         //   RstepCount++;
         // }
-        canStim = true;    // Enable stimulation
+        canStim = true;         // enable stimulation
       }
       break;
 
-    case 3:  // Double stance following left single stance
-      if (LTO) {           // If left toe off, enter right single stance
+    case 3:                     // double stance after left single stance
+      if (LTO) {                // if left toe off, enter right single stance
         phase = 2;
-        LstepCount++;      // Increment left step count
+        LstepCount++;           // increment left step count
         Serial.print("Left Step: ");
         Serial.println(LstepCount);
       }
       break;
 
-    case 4:  // Double stance following right single stance
-      if (RTO) {           // If right toe off, enter left single stance
+    case 4:                     // double stance after right single stance
+      if (RTO) {                // if right toe off, enter left single stance
         phase = 1;
-        RstepCount++;      // Increment right step count
+        RstepCount++;           // increment right step count
         Serial.print("Right Step: ");
         Serial.println(RstepCount);
       }
       break;
   }
 
-  // Right leg stimulation condition
+  // right leg stimulation condition
   if ((!(RstepCount % numStrides)) && phase == 2 && canStim) {
     Serial.println("Right Stimulation Triggered");
     delay(rightDelayTime);
     digitalWrite(rightOutputPin, HIGH);
     digitalWrite(rightViconOut, HIGH);
-    delay(stimPulseDuration);   // Stimulation duration
+    // TODO: consider removing delay here too to allow state machine to
+    // continue running for these 20 ms
+    delay(stimPulseDuration);   // stimulation duration
     digitalWrite(rightOutputPin, LOW);
     digitalWrite(rightViconOut, LOW);
     canStim = false;
   }
 
-  // Left leg stimulation condition
+  // left leg stimulation condition
   if ((!(LstepCount % numStrides)) && phase == 1 && canStim) {
     Serial.println("Left Stimulation Triggered");
     delay(leftDelayTime);
     digitalWrite(leftOutputPin, HIGH);
     digitalWrite(leftViconOut, HIGH);
-    delay(stimPulseDuration);   // Stimulation duration
+    // TODO: consider removing delay here too to allow state machine to
+    // continue running for these 20 ms
+    delay(stimPulseDuration);   // stimulation duration
     digitalWrite(leftOutputPin, LOW);
     digitalWrite(leftViconOut, LOW);
     canStim = false;
