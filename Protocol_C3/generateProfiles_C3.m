3cd1241e4b2ce276d1e1b0af7039babb3cd97602
nwb18 <nwbrantly@pitt.edu>
2024-11-10 10:21:20 -0500

    Remove directory changing code
    Remove code in 'generateProfiles_C3' function that retrieves the working directory changes the directory and then changes back to the working directory at the end to replace it with code to simply save the profile using the full file path. Update comments. Remove the conditional statement for the speed profiles and instead use the minimum of two possible fast speeds before dividing by two for the slow speed to ensure the 2:1 ratio. Add structure to define the number of strides in short trials and long trials. Remove unnecessary variable clearing. These changes are a subset of suggestions from ChatGPT to simplify this function. NWB

--- Protocol_C3/generateProfiles_C3.m
+++ Protocol_C3/generateProfiles_C3.m
@@ -1,6 +1,6 @@
 function dirProfile = generateProfiles_C3(participantID,legSlow, ...
     speedOGMid,speedOGFast)
-%GENERATEPROFILE_C3 Generate all velocity profiles for C3 study participant
+%GENERATEPROFILE_C3 Generate and save velocity profiles for the C3 study
 %   This function takes as input the participant ID, slow leg, middle, and
 % fast walking speeds, and generates MAT files containing the velocities
 % for the left and right legs (i.e., the required speed profiles) for each
@@ -23,56 +23,46 @@ function dirProfile = generateProfiles_C3(participantID,legSlow, ...
 %   profileDir: string or character array of the folder to which the MAT
 %       profile files are saved
 
-dirWork = pwd();                    % get current (working) folder
-% TODO: assuming Windows file system path delimiter, update to be agnostic
+% define base directories and output folder path
 dirBase = ['C:\Users\Public\Documents\MATLAB\ExperimentalGUI\profiles' ...
     '\Stroke_CCC'];
 dirProfile = fullfile(dirBase,participantID,[upper(legSlow) '_Slow']);
 if ~exist(dirProfile,'dir')         % if profile folder does not exist, ...
     mkdir(dirProfile)               % make it
 end
-cd(dirProfile);                     % change working directory
 
+% define speeds with adjustments for thresholds
 speedMean = 0.85 * speedOGMid;      % initialize mid speed to 85% 6MWT
-speedFast = (4/3) * speedMean;      % compute fast speed as 33% greater
-speedSlow = (2/3) * speedMean;      % compute slow speed as 33% lesser
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% COMMENT BELOW CONDITIONAL BLOCK IF PARTICIPANT REPORTS SPEEDS TOO SLOW %%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-if speedFast > 0.85*speedOGFast     % if fast speed exceeds threshold, ...
-    disp('The fast speed conditional has been entered.');
-    speedFast = 0.85 * speedOGFast; % saturate fast speed at threshold, and
-    speedSlow = speedFast / 2;      % set slow speed at half fast speed
-    speedMean = mean([speedFast speedSlow]);    % set mean speed to midpnt
-end
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% fast speed is minimum of 4/3 mean speed or 85% OG fast speed
+speedFast = min((4/3) * speedMean, 0.85 * speedOGFast);
+speedSlow = speedFast / 2;          % compute slow speed for 2:1 ratio
+speedMean = mean([speedFast speedSlow]);    % set mean to midpoint speed
+
+% define profiles and save using the helper function
+numStrides = struct('short',51,'long',151);
 
 % TM Baseline Slow (50 strides) - NOT USED DURING C3 EXPERIMENT
-% velL = speedSlow * ones(51,1);
+% velL = speedSlow * ones(numStrides.short,1);
 % velR = velL;
-% save('TM_Baseline_Slow','velL','velR');
-% clear velL velR;
+% save(fullfile(dirProfile,'TM_Baseline_Slow'),'velL','velR');
 
 % TM Baseline Mid 1 (50 strides)
-velL = speedMean * ones(51,1);
+velL = speedMean * ones(numStrides.short,1);
 velR = velL;
-save('TM_Baseline_Mid1','velL','velR');
-clear velL velR;
+save(fullfile(dirProfile,'TM_Baseline_Mid1'),'velL','velR');
 
 % TM Baseline Fast (50 strides)
-velL = speedFast * ones(51,1);
+velL = speedFast * ones(numStrides.short,1);
 velR = velL;
-save('TM_Baseline_Fast','velL','velR');
-clear velL velR;
+save(fullfile(dirProfile,'TM_Baseline_Fast'),'velL','velR');
 
 % OG Baseline Mid (150 strides)
-velL = speedMean * ones(151,1);
+velL = speedMean * ones(numStrides.long,1);
 velR = velL;
-save('OG_Baseline_Mid','velL','velR');
-clear velL velR;
+save(fullfile(dirProfile,'OG_Baseline_Mid'),'velL','velR');
 
 % TM Short Exposure (Negative, 150 strides)
-velL = speedMean*ones(50,1);        % initial tied-belt mid speed
+velL = speedMean*ones(numStrides.short-1,1);% initial tied-belt mid speed
 velR = velL;
 if strncmpi(legSlow,'r',1)          % if right side slow, ...
     % ramp speed for 20 strides followed by 30 strides at full difference
@@ -83,13 +73,12 @@ else    % otherwise, left side fast, right side slow
     velL = [velL; linspace(speedMean,speedFast,20)'; speedFast*ones(30,1)];
     velR = [velR; linspace(speedMean,speedSlow,20)'; speedSlow*ones(30,1)];
 end
-velL = [velL; speedMean*ones(51,1)];% final tied-belt mid speed
-velR = [velR; speedMean*ones(51,1)];
-save('TM_ShortExposure_Neg','velL','velR');
-clear velL velR;
+velL = [velL; speedMean*ones(numStrides.short,1)];  % final tied-belt mid
+velR = [velR; speedMean*ones(numStrides.short,1)];
+save(fullfile(dirProfile,'TM_ShortExposure_Neg'),'velL','velR');
 
 % TM Short Exposure (Positive, 150 strides)
-velL = speedMean*ones(50,1);        % initial tied-belt mid speed
+velL = speedMean*ones(numStrides.short-1,1);% initial tied-belt mid speed
 velR = velL;
 if strncmpi(legSlow,'r',1)          % if right side slow, ...
     % ramp speed for 20 strides followed by 30 strides at full difference
@@ -100,35 +89,29 @@ else    % otherwise, left side slow, right side fast
     velL = [velL; linspace(speedMean,speedSlow,20)'; speedSlow*ones(30,1)];
     velR = [velR; linspace(speedMean,speedFast,20)'; speedFast*ones(30,1)];
 end
-velL = [velL; speedMean*ones(51,1)];% final tied-belt mid speed
-velR = [velR; speedMean*ones(51,1)];
-save('TM_ShortExposure_Pos','velL','velR');
-clear velL velR;
+velL = [velL; speedMean*ones(numStrides.short,1)];  % final tied-belt mid
+velR = [velR; speedMean*ones(numStrides.short,1)];
+save(fullfile(dirProfile,'TM_ShortExposure_Pos'),'velL','velR');
 
 % TM Baseline Mid 2 (150 strides)
-velL = speedMean * ones(151,1);
+velL = speedMean * ones(numStrides.long,1);
 velR = velL;
-save('TM_Baseline_Mid2','velL','velR');
-clear velL velR;
+save(fullfile(dirProfile,'TM_Baseline_Mid2'),'velL','velR');
 
 % TM Adaptation (150 strides each trial for six trials total)
 if strncmpi(legSlow,'r',1)          % if right side is slow, ...
-    velL = speedFast * ones(151,1);
-    velR = speedSlow * ones(151,1);
+    velL = speedFast * ones(numStrides.long,1);
+    velR = speedSlow * ones(numStrides.long,1);
 else                                % otherwise, left side is slow, ...
-    velL = speedSlow * ones(151,1);
-    velR = speedFast * ones(151,1);
+    velL = speedSlow * ones(numStrides.long,1);
+    velR = speedFast * ones(numStrides.long,1);
 end
-save('TM_Adaptation','velL','velR');
-clear velL velR;
+save(fullfile(dirProfile,'TM_Adaptation'),'velL','velR');
 
 % OG or TM Post-Adaptation (150 strides each trial)
-velL = speedMean * ones(151,1);
+velL = speedMean * ones(numStrides.long,1);
 velR = velL;
-save('PostAdaptation','velL','velR');
-clear velL velR;
-
-cd(dirWork);                        % reset working directory
+save(fullfile(dirProfile,'PostAdaptation'),'velL','velR');
 
 end
 
