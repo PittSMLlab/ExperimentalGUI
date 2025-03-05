64274d121ccbda5ee07647a4634d926ea3792366
Sensorimotor Learning Lab at Pitt <36201963+PittSMLlab@users.noreply.github.com>
2025-03-04 20:28:42 -0500

    Use switch statement
    Modify H-reflex Arduino stimulation code to stimulate upon command from MATLAB to use a 'switch' statement to match the new gait event state machine Arduino stimulation code. Increase baud rate to match faster rate in other code. Update comments and code formatting. Remove print to serial window since unable to see while running MATLAB anyway. Initialize 'value' variable outside of main loop. Remove unnecessary conditional '> 0'. NOTE that this code has been tested with a 'ctrltrain' profile on the treadmill and verified that it is working as expected and desired. NWB

--- HreflexStimArduino/Dual_Stim_Matlab/Dual_Stim_Matlab.ino
+++ HreflexStimArduino/Dual_Stim_Matlab/Dual_Stim_Matlab.ino
@@ -3,47 +3,45 @@
 // date (started): 20 Mar. 2024
 // author(s): SL, NWB
 
-// threshold values
-const int stimDuration = 20;      // stimulation pulse duration (ms)
+// stimulation pulse duration (ms)
+const int stimDuration = 20;
 
-// right stimulator configuration
+// stimulator configuration
 const int rightOutputPin = 8;
 const int rightViconOut = 11;
-
-// left stimulator configuration
 const int leftOutputPin = 9;
 const int leftViconOut = 12;
+int value = 0;  // serial communication integer
 
 void setup() {
-  // increased baud rate for faster serial communication
-  Serial.begin(9600);
+  Serial.begin(115200);
   // ensure pins are properly set for output
   pinMode(rightOutputPin, OUTPUT);
   pinMode(rightViconOut, OUTPUT);
   pinMode(leftOutputPin, OUTPUT);
   pinMode(leftViconOut, OUTPUT);
-
-  Serial.println("\n\n\n\nSTART");
 }
 
 void loop() {
-  if (Serial.available() > 0) {
-    int value = Serial.read();
-    if (value == 1) {       // stimulate right leg
-      Serial.println("Entered Right Stim Loop");
-      digitalWrite(rightOutputPin, HIGH);
-      digitalWrite(rightViconOut, HIGH);
-      delay(stimDuration);
-      digitalWrite(rightOutputPin, LOW);
-      digitalWrite(rightViconOut, LOW);
-    } 
-    else if (value == 2) {  // stimulate left leg
-      Serial.println("Entered Left Stim Loop");
-      digitalWrite(leftOutputPin, HIGH);
-      digitalWrite(leftViconOut, HIGH);
-      delay(stimDuration);
-      digitalWrite(leftOutputPin, LOW);
-      digitalWrite(leftViconOut, LOW);
+  if (Serial.available()) {
+    value = Serial.read();
+    switch (value)
+    {
+      case 1:   // stimulate right leg
+        digitalWrite(rightOutputPin, HIGH);
+        digitalWrite(rightViconOut, HIGH);
+        delay(stimDuration);
+        digitalWrite(rightOutputPin, LOW);
+        digitalWrite(rightViconOut, LOW);
+        break;
+
+      case 2:   // stimulate left leg
+        digitalWrite(leftOutputPin, HIGH);
+        digitalWrite(leftViconOut, HIGH);
+        delay(stimDuration);
+        digitalWrite(leftOutputPin, LOW);
+        digitalWrite(leftViconOut, LOW);
+        break;
     }
   }
 }
