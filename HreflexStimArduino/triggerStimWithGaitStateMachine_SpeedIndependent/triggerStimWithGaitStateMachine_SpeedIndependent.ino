a2a03bd46c72fe4cb5ee0b98f8a04a4928c4d796
nwb18 <nwbrantly@pitt.edu>
2025-03-16 16:37:22 -0400

    Rearrange Arduino variables
    Rearrange and reorganize Arduino H-reflex stimulation code variables into smaller sections for clarity. NWB

--- HreflexStimArduino/triggerStimWithGaitStateMachine_SpeedIndependent/triggerStimWithGaitStateMachine_SpeedIndependent.ino
+++ HreflexStimArduino/triggerStimWithGaitStateMachine_SpeedIndependent/triggerStimWithGaitStateMachine_SpeedIndependent.ino
@@ -8,26 +8,55 @@
 // date (started): 26 Mar. 2024
 // author(s): SL, NWB
 
-// initialize variables to track gait events and phases
+// -------------------------- Pin Definitions --------------------------
+const int pinInFzL = A0;     // left force plate sensor input
+const int pinOutStimL = 9;   // left stimulation output
+const int pinOutViconL = 12; // left Vicon output
+const int pinInFzR = A1;     // right force plate sensor input
+const int pinOutStimR = 8;   // right stimulation output
+const int pinOutViconR = 11; // right Vicon output
+
+// -------------------------- Constants & Parameters --------------------------
+// z-axis force threshold in DAQ bits (estimated by observing the z-axis
+// force plate voltages during walking and converting to bits based on 10-bit
+// ADC; we also verified that the Arduino-read bits are comparable to what we
+// expect given the Vicon-read voltages, although we found a small bias)
+// TODO: it may be necessary to increase to account for left FP noise and
+// higher baud rate in Arduino than in MATLAB (more susceptible to false gait
+// event detection))
+const int threshFzUp = 30;              // force threshold (bits) for detecting stance (upper)
+const int threshFzDown = 2;             // force threshold for detecting stance (lower)
+const int durStimPulse = 20;            // stimulation pulse duration (ms)
+const unsigned long timeDebounce = 100; // de-bouncing time constant (ms)
+const float percentSS2Stim = 0.50;      // 50% of single stance phase target
+const float alpha = 0.7;                // smoothing factor (0 < alpha <= 1)
+
+// -------------------------- Global Variables --------------------------
+// gait event state variables
 bool isCurrStanceL = false; // is current step left foot stance?
 bool isCurrStanceR = false; // is current step right foot stance?
 bool isPrevStanceL = false; // is previous step left foot stance?
 bool isPrevStanceR = false; // is previous step right foot stance?
-// int freqStim = 1;                    // temporary stride frequency of stimulation for development
-bool LHS = false;                    // is there a left heel strike event?
-bool RHS = false;                    // is there a right heel strike event?
-bool LTO = false;                    // is there a left toe off event?
-bool RTO = false;                    // is there a right toe off event?
+bool LHS = false;           // is there a left heel strike event?
+bool RHS = false;           // is there a right heel strike event?
+bool LTO = false;           // is there a left toe off event?
+bool RTO = false;           // is there a right toe off event?
+
+// timing variables (in ms)
 unsigned long timeLHS = 0;           // time of most recent LHS events
 unsigned long timeRHS = 0;           // time of most recent RHS events
 unsigned long timeLTO = 0;           // time of most recent LTO events
 unsigned long timeRTO = 0;           // time of most recent RTO events
 unsigned long timeSinceLTO = 0;      // time since LTO event
 unsigned long timeSinceRTO = 0;      // time since RTO event
-unsigned long timeTargetStimL = 198; // initialize RTO delay to 198 ms
-unsigned long timeTargetStimR = 198; // initialize LTO delay to 198 ms
-float percentSS2Stim = 0.50;         // percentage of single stance phase
-float alpha = 0.7;                   // smoothing factor (0 < alpha <= 1)
+unsigned long timeStanceChangeL = 0; // time when isCurrentStance changes
+unsigned long timeStanceChangeR = 0;
+unsigned long timeSinceStanceChangeL = 0; // time elapsed since isCurrentStance changed
+unsigned long timeSinceStanceChangeR = 0;
+unsigned long timeTargetStimL = 198; // initial RTO delay estimate for left stimulation
+unsigned long timeTargetStimR = 198; // initial LTO delay estimate for right stimulation
+
+// single stance duration estimates & measured durations
 // NOTE: initial estimate set based on normative data from Liu et al. 2014
 // (Gait & Posture), which includes mean "normal" velocity of 1.17+/-0.14 m/s
 // (N=95, 34.9+/-11.8 years), mean gait cycle duration of 1062.02 ms, and
@@ -36,10 +65,13 @@ float alpha = 0.7;                   // smoothing factor (0 < alpha <= 1)
 // comparable to those of Hebenstreit et al. 2015 (Human Movement Science).
 // Using a better initial estimate may improve the stimulation timing
 // precision of the first few strides within a trial.
-float estSSL = 396.6;       // estimated single stance duration left (initially 396.6 ms)
-float estSSR = 396.6;       // estimated single stance duration right (initially 396.6 ms)
-unsigned long durSSL = 397; // left single stance duration
-unsigned long durSSR = 397; // right single stance duration
+float estSSL = 396.6; // estimated single stance duration (ms)
+float estSSR = 396.6;
+unsigned long durSSL = 397; // measured single stance duration (ms)
+unsigned long durSSR = 397;
+
+// stimulation control flags
+// int freqStim = 1;                 // temporary stride frequency of stimulation for development
 // bool canStimL = false;            // is stimulation allowed at this time?
 // bool canStimR = false;            // is stimulation allowed at this time?
 bool shouldStimL = false;         // should stimulate left leg this stride?
@@ -49,11 +81,8 @@ bool isStimmingL = false;         // is the left stimulator currently on?
 bool isStimmingR = false;         // is the right stimulator currently on?
 unsigned long timeStimStartL = 0; // time when left stimulation started
 unsigned long timeStimStartR = 0; // time when right stimulation started
-unsigned long timeStanceChangeL = 0;
-unsigned long timeStanceChangeR = 0;      // time when isCurrentStance changes
-unsigned long timeSinceStanceChangeL = 0; // time elapsed since isCurrentStance changed
-unsigned long timeSinceStanceChangeR = 0;
 
+// gait phase & step counts
 // gait phase: 0 = initial double support, 1 = single L support, 2 = single R
 // support, 3 = double support from single L support, 4 = double support from
 // single R support
@@ -61,27 +90,8 @@ int phase = 0;
 int numStepsL = 0; // left step counter
 int numStepsR = 0; // right step counter
 
-// z-axis force threshold in DAQ bits (estimated by observing the z-axis
-// force plate voltages during walking and converting to bits based on 10-bit
-// ADC; we also verified that the Arduino-read bits are comparable to what we
-// expect given the Vicon-read voltages, although we found a small bias)
-// TODO: it may be necessary to increase to account for left FP noise and
-// higher baud rate in Arduino than in MATLAB (more susceptible to false gait
-// event detection))
-const int threshFzUp = 30; // force threshold bits to detect stance phase
-const int threshFzDown = 2;
-const int durStimPulse = 20;           // stimulation pulse duration (ms)
-const unsigned long timeDebounce = 50; // de-bouncing time constant
-
-// right and left stimulator pin configurations
-const int pinInFzL = A0;
-const int pinOutStimL = 9;
-const int pinOutViconL = 12;
-const int pinInFzR = A1;
-const int pinOutStimR = 8;
-const int pinOutViconR = 11;
-
-int command = 0; // serial communication integer
+// serial communication variable
+int command = 0;
 
 // -------------------------- Setup --------------------------
 void setup()
